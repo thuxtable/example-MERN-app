96de50a02ebedc8d5f9e91c252b8198d8e4b6e81
Tyler Huxtable <tshuxtable@gmail.com>
2022-09-05 13:31:43 -0700

    Routing set up, but POST throwing errors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyparser from 'body-parser';
+import routes from './routes/soccerRoutes';
 
 const app = express();
 const PORT = 3000;
 
+// mongo connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb+srv://thuxtable:28zen8Zp4q4qQLy@cluster0.hm0cq8i.mongodb.net/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// bodyparser setup
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(bodyparser.json());
+
+routes(app);
+
 app.get('/', (req, res) =>
-    res.send(`Our Soccer application is running ${PORT}`)
+    res.send(`Our Soccer application is running on port ${PORT}`)
 );
 
 app.listen(PORT, () =>
