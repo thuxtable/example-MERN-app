caac52ac354dc0f08c21e2ae4792d5e8e0c82b82
Tyler Huxtable <tshuxtable@gmail.com>
2022-09-07 22:09:17 -0700

    Backend complete on port 4000; starting frontend

--- backend/routes/soccerRoutes.js
+++ backend/routes/soccerRoutes.js
@@ -1,9 +1,19 @@
-import {addNewPlayer} from '../controllers/playerControllers';
+import {addNewPlayer, getPlayers, getPlayerWithID, updatePlayer, deletePlayer} from '../controllers/playerControllers';
 
 const routes = (app) => {
     app.route('/players')
+    // GET endpoint
+        .get(getPlayers)
     // POST endpoint
         .post(addNewPlayer);
+
+    app.route('/player/:PlayerId')
+    // GET specific player    
+        .get(getPlayerWithID)
+    // PUT update a specific player
+        .put(updatePlayer)
+    // DELETE specific player
+        .delete(deletePlayer);
 }
 
 export default routes;
